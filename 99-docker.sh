f5d5e5c41865099aa88ec017ead3599f0f346c93
Luiz Pereira de Souza Filho <lpsouza@gmail.com>
2021-05-31 21:12:23 -0300

    Changes to use docker like non superuser

--- 99-docker.sh
+++ 99-docker.sh
@@ -2,3 +2,7 @@
 
 # Install Docker
 curl https://get.docker.com | sh
+
+# Change docker to execute via non superuser
+sudo usermod -aG docker $USER
+sudo setfacl -m user:$USER:rw /var/run/docker.sock
