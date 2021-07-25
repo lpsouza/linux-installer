53649203df4f082db0fb002494218c514ad63ed1
Luiz Pereira de Souza Filho <lpsouza@gmail.com>
2021-07-25 17:02:42 -0300

    Added $HOME directory

--- 99-keychain.sh
+++ 99-keychain.sh
@@ -2,5 +2,5 @@
 
 # Keychain
 sudo apt install -y keychain
-echo "/usr/bin/keychain --nogui --quiet $HOME/.ssh/id_rsa" >> .bashrc
-echo "source $HOME/.keychain/$HOSTNAME-sh" >> .bashrc
+echo "/usr/bin/keychain --nogui --quiet $HOME/.ssh/id_rsa" >> $HOME/.bashrc
+echo "source $HOME/.keychain/$HOSTNAME-sh" >> $HOME/.bashrc
