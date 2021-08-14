e40add66f90740e9980a9dda712b4b014c305913
Luiz Pereira de Souza Filho <lpsouza@gmail.com>
2021-08-14 20:24:40 -0300

    Change nvm.sh script

--- basic/nvm.sh
+++ basic/nvm.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 # NVM (NodeJS)
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | sh
 export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && . "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 nvm install --lts
 nvm use --lts
+npm install -g npm
 npm install -g jshint
