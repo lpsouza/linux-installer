05d824271cfa160e095f48da881b91e7e3194a57
Luiz Pereira de Souza Filho <lpsouza@gmail.com>
2021-09-18 13:47:01 -0300

    Added Jekyll

---
+++ basic/jekyll.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Jekyll
+sudo apt-get install -y ruby-full zlib1g-dev
+echo '# Install Ruby Gems to $HOME/gems' >> $HOME/.bashrc
+echo 'export GEM_HOME="$HOME/gems"' >> $HOME/.bashrc
+echo 'export PATH="$HOME/gems/bin:$PATH"' >> $HOME/.bashrc
+source $HOME/.bashrc
+gem install jekyll bundler
