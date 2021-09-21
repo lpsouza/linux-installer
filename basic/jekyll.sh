24225f01a5bae5f281905440964c09d8dea3d0d0
Luiz Pereira de Souza Filho <lpsouza@gmail.com>
2021-09-21 02:40:24 -0300

    Added jekyll to basic setup

--- basic/jekyll.sh
+++ basic/jekyll.sh
@@ -3,6 +3,7 @@
 # Jekyll
 sudo apt-get install -y ruby-full zlib1g-dev
 echo 'export GEM_HOME="$HOME/.gems"' >> $HOME/.bashrc
-echo 'export PATH="$HOME/.gems/bin:$PATH"' >> $HOME/.bashrc
-source $HOME/.bashrc
+echo 'export PATH="$PATH:$HOME/.gems/bin"' >> $HOME/.bashrc
+export GEM_HOME="$HOME/.gems"
+export PATH="$PATH:$HOME/.gems/bin"
 gem install jekyll bundler
