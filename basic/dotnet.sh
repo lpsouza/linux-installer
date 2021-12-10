4ccde10e89703e4be92d91471513ca8093ff9ae2
Luiz Souza <lpsouza@gmail.com>
2021-12-10 16:35:55 -0300

    Upgrade dotnet version

--- basic/dotnet.sh
+++ basic/dotnet.sh
@@ -4,5 +4,5 @@
 wget https://packages.microsoft.com/config/debian/10/packages-microsoft-prod.deb -4 -O packages-microsoft-prod.deb
 sudo dpkg -i packages-microsoft-prod.deb
 sudo apt update
-sudo apt install -y dotnet-sdk-5.0
+sudo apt install -y dotnet-sdk-6.0
 rm packages-microsoft-prod.deb
