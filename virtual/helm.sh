c729d678890ffa43ab33e32f481a497140ce89fe
Luiz Souza <lpsouza@gmail.com>
2022-05-17 19:14:30 -0300

    Create helm.sh

---
+++ virtual/helm.sh
@@ -0,0 +1,4 @@
+curl https://baltocdn.com/helm/signing.asc | sudo apt-key add -
+echo "deb https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
+sudo apt-get update
+sudo apt-get install -y helm
