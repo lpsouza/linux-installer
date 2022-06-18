d63d3fd3e81490ab05c139062305090fba2e1cfb
Luiz Souza <lpsouza@gmail.com>
2022-06-18 00:22:53 -0300

    Create github-cli.sh

---
+++ basic/github-cli.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Github CLI
+curl -O https://github.com/cli/cli/releases/download/v2.12.1/gh_2.12.1_linux_amd64.deb
+sudo dpkg -i gh_2.12.1_linux_amd64.deb
+rm gh_2.12.1_linux_amd64.deb
