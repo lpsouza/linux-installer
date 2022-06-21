68d7bdc782e1564071bead3deccbc238ba9cc1cd
Luiz Pereira de Souza Filho <lpsouza@gmail.com>
2022-06-21 00:33:38 -0300

    Added vim.sh installer

---
+++ basic/vim.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+# vim
+sudo apt install vim
+curl https://gist.githubusercontent.com/lpsouza/5dded5c48d62fb9eac6cc0d10609d96f/raw/d29735d8acfa329916bf1699fdd8383f4efc12ea/.vimrc -o $HOME/.vimrc
