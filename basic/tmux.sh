9d72981040f119070690fc996d855066cf0e5a68
Luiz Pereira de Souza Filho <lpsouza@gmail.com>
2022-06-21 21:09:01 -0300

    Escape the variable TMUX

--- basic/tmux.sh
+++ basic/tmux.sh
@@ -16,7 +16,7 @@ else
 
     sudo apt install tmux -y
     echo "# Tmux autostart" >> $HOME/.bashrc
-    echo "if [ -z \"$TMUX\" ]; then tmux a || tmux; fi" >> $HOME/.bashrc
+    echo "if [ -z \"\$TMUX\" ]; then tmux a || tmux; fi" >> $HOME/.bashrc
     curl https://gist.githubusercontent.com/lpsouza/6534fd264197963e86dce01ece9f63d1/raw/c36e4fc56f89c1bc4cffd002b3078b1bc2e932da/.tmux.conf -o $HOME/.tmux.conf
 
     [[ ! -d "$HOME/.config" ]] && mkdir $HOME/.config
