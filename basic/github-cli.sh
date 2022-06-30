7aabbff7bdaa7bc8752bd8ac8a0894ba552d8ed1
Luiz Pereira de Souza Filho <lpsouza@gmail.com>
2022-06-30 12:36:09 -0300

    Change curl to wget to get file from github

--- basic/github-cli.sh
+++ basic/github-cli.sh
@@ -14,7 +14,7 @@ if [ "$INSTALL_ALREADY" == true ]; then
 else
     echo "Installing $INSTALL_NAME."
 
-    curl -O https://github.com/cli/cli/releases/download/v2.12.1/gh_2.12.1_linux_amd64.deb
+    wget https://github.com/cli/cli/releases/download/v2.12.1/gh_2.12.1_linux_amd64.deb
     sudo dpkg -i gh_2.12.1_linux_amd64.deb
     rm gh_2.12.1_linux_amd64.deb
 
