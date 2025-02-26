29b79f1e898613c0fd8410cfb98db2ecbdd7d4ed
Luiz Pereira de Souza Filho <lpsouza@gmail.com>
2025-02-26 14:23:46 -0300

    Add FUSE support to test container for Ansible playbook testing

--- tests/test.sh
+++ tests/test.sh
@@ -7,4 +7,4 @@ cd ..
 docker build -t test-ansible -f tests/Dockerfile ./tests
 
 # Run the test container
-docker run --rm -it -v $PWD:/home/test/projects/linux-installer --name test-ansible test-ansible bash
+docker run --rm -it -v $PWD:/home/test/projects/linux-installer --device /dev/fuse --cap-add SYS_ADMIN --name test-ansible test-ansible bash
